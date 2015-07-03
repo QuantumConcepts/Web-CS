006fe186bc0ef9aef4bcf49874972d4db89b4e7d
Josh McCullough <joshua.mccullough@gmail.com>
2015-07-03 14:20:58 -0400

    Fixed up gulpfile.

--- Source/Web/gulpfile.js
+++ Source/Web/gulpfile.js
@@ -1,23 +1,24 @@
 /// <binding Clean='clean' />
 'use strict';
+
 var gulp = require("gulp"),
-  rimraf = require("rimraf"),
-  concat = require("gulp-concat"),
-  cssmin = require("gulp-cssmin"),
-  uglify = require("gulp-uglify"),
-  project = require("./project.json");
+    rimraf = require("rimraf"),
+    concat = require("gulp-concat"),
+    cssmin = require("gulp-cssmin"),
+    uglify = require("gulp-uglify"),
+    project = require("./project.json");
 
 var paths = function () {
   var webroot = "./" + project.webroot + "/";
   
   return {
     webroot: webroot,
-    paths.js = paths.webroot + "js/**/*.js";
-    paths.minJs = paths.webroot + "js/**/*.min.js";
-    paths.css = paths.webroot + "css/**/*.css";
-    paths.minCss = paths.webroot + "css/**/*.min.css";
-    paths.concatJsDest = paths.webroot + "js/site.min.js";
-    paths.concatCssDest = paths.webroot + "css/site.min.css";
+    js: paths.webroot + "js/**/*.js",
+    minJs: paths.webroot + "js/**/*.min.js",
+    css: paths.webroot + "css/**/*.css",
+    minCss: paths.webroot + "css/**/*.min.css",
+    concatJsDest: paths.webroot + "js/site.min.js",
+    concatCssDest: paths.webroot + "css/site.min.css"
   };
 }();
 
